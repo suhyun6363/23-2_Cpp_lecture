2028358e99ca5035c857010098be8dde676d6738
suhyun__ <thebbbb1206@gmail.com>
2023-10-25 00:22:29 +0900

    cpp 파일 수정

--- chapter2/TemplateFuncApp/TemplateFuncApp.cpp
+++ chapter2/TemplateFuncApp/TemplateFuncApp.cpp
@@ -3,7 +3,15 @@ using std::cout;
 using std::endl;
 
 template <typename T>
-T max(T a, T b)
+const T& max(const T& a, const T& b)
+{
+	if (a > b)
+		return a;
+	return b;
+}
+
+template <typename T1, typename T2>
+const T1& max(const T1& a, const T2& b)
 {
 	if (a > b)
 		return a;
@@ -20,6 +28,9 @@ int main()
 	float y = 250.57f;
 	float z = max(x, y);
 	cout << "두 실수 중 큰 값은 " << z << "입니다." << endl;
+
+	int m = max(i, x); //(int, float) - 두 개의 템플릿 파라미터를 사용하는 max 템플릿 함수를 사용해야 함
+	cout << "두 실수 중 큰 값은 " << m << "입니다." << endl;
 
 	return 0;
 }
